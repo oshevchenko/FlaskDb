d1f9d49553a2ccb8f05b080926d07e6b38320799
oshevchenko <shevchenko.adb@gmail.com>
2021-03-12 16:07:26 +0200

    Create article implementation.
    Article created in the webform and added to the SQL database.
    
    Signed-off-by: oshevchenko <shevchenko.adb@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from markupsafe import escape
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
@@ -13,6 +28,23 @@ def index():
 @app.route("/about")
 def hello():
     return render_template("about.html")
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error while adding article."
+    else:
+        return render_template("create-article.html")
 
 
 @app.route('/user/<username>')
